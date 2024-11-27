6ac0ac81bb6cf9e3a009ba0e545ee1a01c62bcb3
Lim Swee Tat <st_lim@stlim.net>
2024-11-27 19:15:05 +0800

    Adding sendmail with lookupmx and newconn

---
+++ internal/sendmail/sendmail.go
@@ -0,0 +1,111 @@
+package sendmail
+
+import (
+	"context"
+	"net"
+
+	"github.com/mjl-/adns"
+	"github.com/mjl-/mox/dns"
+	"github.com/rs/zerolog"
+	"github.com/stlimtat/remiges-smtp/internal/utils"
+)
+
+const (
+	DEFAULT_SMTP_PORT_STR string = "25"
+)
+
+//go:generate mockgen -destination=sendmail_mock.go -package=sendmail . INetDialer,IResolver,IMailSender
+type IResolver interface {
+	LookupMX(ctx context.Context, name string) ([]*net.MX, adns.Result, error)
+}
+
+type INetDialer interface {
+	Dial(network, address string) (net.Conn, error)
+}
+
+type IMailSender interface {
+	LookupMX(ctx context.Context, domain dns.Domain) ([]string, error)
+	NewConn(ctx context.Context) (net.Conn, error)
+}
+
+type MXRecord struct {
+	ADNSResult adns.Result
+	Domain     string
+	Entries    []*net.MX
+	Hosts      []string
+}
+
+type MailSender struct {
+	CachedMX map[string]MXRecord
+	Dialer   INetDialer
+	Resolver IResolver
+}
+
+func NewMailSender(
+	_ context.Context,
+	dialer INetDialer,
+	resolver IResolver,
+) *MailSender {
+	result := &MailSender{
+		CachedMX: make(map[string]MXRecord, 0),
+		Dialer:   dialer,
+		Resolver: resolver,
+	}
+	return result
+}
+
+func (m *MailSender) LookupMX(
+	ctx context.Context,
+	domain dns.Domain,
+) ([]string, error) {
+	logger := zerolog.Ctx(ctx).
+		With().
+		Str("domain", domain.ASCII).
+		Logger()
+	// 1. check if mx already exists in cache
+	mxRecord, ok := m.CachedMX[domain.ASCII]
+	if !ok {
+		// 2. resolve the mx record for the domain
+		mxList, aDNSResult, err := m.Resolver.LookupMX(ctx, domain.ASCII)
+		if err != nil {
+			logger.Error().Err(err).Msg("m.Resolver.LookupMX")
+			return nil, err
+		}
+		m.CachedMX[domain.ASCII] = MXRecord{
+			ADNSResult: aDNSResult,
+			Domain:     domain.ASCII,
+			Entries:    mxList,
+			Hosts:      make([]string, 0),
+		}
+		mxRecord = m.CachedMX[domain.ASCII]
+		for _, entry := range mxRecord.Entries {
+			mxRecord.Hosts = append(mxRecord.Hosts, entry.Host)
+		}
+	}
+	logger.Info().
+		Strs("hosts", mxRecord.Hosts).
+		Msg("lookupMX")
+	return mxRecord.Hosts, nil
+}
+
+func (m *MailSender) NewConn(
+	ctx context.Context,
+	hosts []string,
+) (net.Conn, error) {
+	logger := zerolog.Ctx(ctx).With().Strs("hosts", hosts).Logger()
+	var err error
+	randomInt, err := utils.RandInt(int64(len(hosts)))
+	if err != nil {
+		logger.Error().Err(err).Msg("utils.RandInt")
+		return nil, err
+	}
+	host := hosts[randomInt]
+
+	addr := net.JoinHostPort(host, DEFAULT_SMTP_PORT_STR)
+	result, err := m.Dialer.Dial("tcp", addr)
+	if err != nil {
+		logger.Error().Err(err).Msg("d.Dial")
+		return nil, err
+	}
+	return result, nil
+}
---
+++ internal/sendmail/sendmail_test.go
@@ -0,0 +1,105 @@
+package sendmail
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"testing"
+
+	"github.com/mjl-/adns"
+	"github.com/mjl-/mox/dns"
+	"github.com/stlimtat/remiges-smtp/internal/telemetry"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+)
+
+func TestLookupMX(t *testing.T) {
+	type LookupMXResult struct {
+		mxList     []*net.MX
+		ADNSResult adns.Result
+		err        error
+	}
+	var tests = []struct {
+		name     string
+		domain   dns.Domain
+		mxResult LookupMXResult
+		wantErr  bool
+	}{
+		{
+			name: "happy",
+			domain: dns.Domain{
+				ASCII: "abc.com",
+			},
+			mxResult: LookupMXResult{
+				mxList: []*net.MX{
+					{Host: "host1.abc.com", Pref: uint16(0)},
+					{Host: "host2.abc.com", Pref: uint16(0)},
+				},
+				ADNSResult: adns.Result{},
+				err:        nil,
+			},
+			wantErr: false,
+		},
+	}
+	// The execution loop
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			ctx, _ = telemetry.InitLogger(ctx)
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			resolver := NewMockIResolver(ctrl)
+			resolver.EXPECT().
+				LookupMX(gomock.Any(), tt.domain.ASCII).
+				DoAndReturn(func(_ context.Context, domain string) ([]*net.MX, adns.Result, error) {
+					assert.Equal(t, domain, tt.domain.ASCII)
+					return tt.mxResult.mxList, tt.mxResult.ADNSResult, tt.mxResult.err
+				})
+
+			m := NewMailSender(ctx, nil, resolver)
+			_, err := m.LookupMX(ctx, tt.domain)
+			if tt.wantErr {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestNewConn(t *testing.T) {
+	var tests = []struct {
+		name    string
+		hosts   []string
+		wantErr bool
+	}{
+		{"happy", []string{"aspmx.l.google.com"}, false},
+	}
+	// The execution loop
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			ctx, _ = telemetry.InitLogger(ctx)
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			dialer := NewMockINetDialer(ctrl)
+			dialer.EXPECT().Dial(gomock.Any(), gomock.Any()).
+				DoAndReturn(func(network, address string) (net.Conn, error) {
+					assert.Equal(t, "tcp", network)
+					hosts2 := []string{}
+					for _, host := range tt.hosts {
+						host = fmt.Sprintf("%s:%s", host, DEFAULT_SMTP_PORT_STR)
+						hosts2 = append(hosts2, host)
+					}
+					assert.Contains(t, hosts2, address)
+					return &net.TCPConn{}, nil
+				})
+
+			m := NewMailSender(ctx, dialer, nil)
+			_, err := m.NewConn(ctx, tt.hosts)
+			if tt.wantErr {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
