fa0d19d07a1087a2103a85e5a6ec017f1c5af0d9
Lim Swee Tat <st_lim@stlim.net>
2025-03-07 15:15:26 +0800

    Removing CachedMX

--- internal/dns/resolver.go
+++ internal/dns/resolver.go
@@ -12,8 +12,7 @@ import (
 
 type Resolver struct {
 	dns.Resolver
-	CachedMX map[string]dn.MXRecord
-	Slogger  *slog.Logger
+	Slogger *slog.Logger
 }
 
 func NewResolver(
@@ -25,7 +24,6 @@ func NewResolver(
 		Resolver: resolver,
 		Slogger:  slogger,
 	}
-	result.CachedMX = make(map[string]dn.MXRecord)
 	return result
 }
 
@@ -37,42 +35,39 @@ func (r *Resolver) LookupMX(
 		With().
 		Str("domain", domain.ASCII).
 		Logger()
+	var result dn.MXRecord
 
-	// 1. check if mx already exists in cache
-	result, ok := r.CachedMX[domain.ASCII]
-	if !ok {
-		// 2. resolve the mx record for the domain
-		ipDomain := dns.IPDomain{
-			Domain: domain,
-		}
+	// 2. resolve the mx record for the domain
+	ipDomain := dns.IPDomain{
+		Domain: domain,
+	}
 
-		_, _, _, expandedNextHop, hosts, _, err := smtpclient.GatherDestinations(
-			ctx, r.Slogger, r.Resolver, ipDomain,
-		)
-		if err != nil {
-			logger.Error().Err(err).Msg("smtpclient.GatherDestinations")
-			return nil, err
-		}
-		// 3. convert from dns.IPDomain to string
-		hostStrSlice := []string{}
-		for _, host := range hosts {
-			hostStrSlice = append(hostStrSlice, host.String())
-		}
-		if expandedNextHop.ASCII != domain.ASCII {
-			r.CachedMX[expandedNextHop.ASCII] = dn.MXRecord{
-				Domain:  expandedNextHop.ASCII,
-				Entries: hosts,
-				Hosts:   hostStrSlice,
-			}
-		}
-		r.CachedMX[domain.ASCII] = dn.MXRecord{
-			Domain:  domain.ASCII,
+	_, _, _, expandedNextHop, hosts, _, err := smtpclient.GatherDestinations( //nolint:dogsled // none of the identifiers are used
+		ctx, r.Slogger, r.Resolver, ipDomain,
+	)
+	if err != nil {
+		logger.Error().Err(err).Msg("smtpclient.GatherDestinations")
+		return nil, err
+	}
+	// 3. convert from dns.IPDomain to string
+	hostStrSlice := []string{}
+	for _, host := range hosts {
+		hostStrSlice = append(hostStrSlice, host.String())
+	}
+	if expandedNextHop.ASCII != domain.ASCII {
+		result = dn.MXRecord{
+			Domain:  expandedNextHop.ASCII,
 			Entries: hosts,
 			Hosts:   hostStrSlice,
 		}
-		result = r.CachedMX[domain.ASCII]
+	}
+	result = dn.MXRecord{
+		Domain:  domain.ASCII,
+		Entries: hosts,
+		Hosts:   hostStrSlice,
 	}
 	logger.Info().
+		Interface("result", result).
 		Strs("hosts", result.Hosts).
 		Msg("lookupMX")
 	return result.Hosts, nil
